040769b63a39ca1f763d984d75b2fd4c4df28c42
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-07 00:23:01 +0300

    в файле index.js прописываем const

---
+++ index.js
@@ -0,0 +1,19 @@
+const PORT = 9001
+
+const express = require ('express')
+const cors = require ('cors')
+const mongoose = require ('mongoose')
+const jwt = require ('jsonwebtoken')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const start = () => {
+    try {
+        app.listen(PORT, () => console.log(`сервер запущен на ${PORT} порте`));
+    } catch(e) {
+        console.log(e);
+    }
+}
