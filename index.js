069a8ef225df58e803a7a0a248495d056f6d7da9
Andrei.Makash <andrei.makash@yandex.ru>
2024-02-07 16:11:58 +0300

    в файле index.js (beckend) app.get('/products',  (req, res)=> {const products = [вставляем масив товаров с main

--- index.js
+++ index.js
@@ -22,6 +22,22 @@ app.post('/registration',  (req, res) => {
             message: 'Вы успешно авторизованы !!!'
         })
     })
+    app.get('/products',  (req, res) => {
+        const products = [
+
+            {id:1,  header: 'Товар 1',image: image, price: 12000 }, 
+            {id:2,  header: 'Товар 2',image: image_2, price: 14000 },
+            {id:3,  header: 'Товар 3',image: image_3, price: 16000 },  
+            {id:4,  header: 'Товар 4',image: image_4, price: 18000 }, 
+            {id:5,  header: 'Товар 5',image: image_5, price: 20000 }, 
+            {id:6,  header: 'Товар 6',image: image_6, price: 22000 }, 
+            {id:7,  header: 'Товар 7',image: image_7, price: 25000 } 
+            
+          ]
+        res.json({
+            data: products
+        })
+    })
 
 const start = () => {
     try {
